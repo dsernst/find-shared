7d6409f63b395cdbf7206955ff66de6fbf6ada2f
David Ernst <git@dsernst.com>
2025-03-12 16:38:40 -0500

    Pull <Footer /> out into own file, customize from template

---
+++ app/Footer.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+export const Footer = () => (
+  <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <a
+      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      href="https://github.com/dsernst/find-shared"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        aria-hidden
+        src="/file.svg"
+        alt="File icon"
+        width={16}
+        height={16}
+      />
+      Github
+    </a>
+    <a
+      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      href="/about"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        aria-hidden
+        src="/window.svg"
+        alt="Window icon"
+        width={16}
+        height={16}
+      />
+      About
+    </a>
+    <a
+      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      href="/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        aria-hidden
+        src="/globe.svg"
+        alt="Globe icon"
+        width={16}
+        height={16}
+      />
+      Link to this page
+    </a>
+  </footer>
+)
