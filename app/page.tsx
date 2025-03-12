8499b0a6384e890bd477efad10eebf1e36a89135
David Ernst <git@dsernst.com>
2025-03-12 17:29:12 -0500

    Begin replacing template boilerplate with our app's custom layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,28 @@
-import Image from 'next/image'
 import { Footer } from './Footer'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
+        {/* Header*/}
+        <div className="text-center w-full">
+          <h1 className="text-4xl font-bold">Find Shared</h1>
+          <h2>Privately mark interests</h2>
+          <h3 className="opacity-50">
+            ...only <i>mutual overlaps</i> get revealed
+          </h3>
         </div>
+
+        {/* Step 1: Add items */}
+        <div>
+          <h4>Step 1: Add your list of possible items, one per line:</h4>
+          <textarea className="w-full h-24 border border-green-800 rounded-md p-2" />
+          <button className="bg-green-800 text-white px-4 py-2 rounded-md">
+            Save
+          </button>
+        </div>
+
+        <div className="">Step 2: Invite collaborators</div>
       </main>
       <Footer />
     </div>
