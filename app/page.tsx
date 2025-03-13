07fcb19c033cfaee0eca87d202421e00ebd24e22
David Ernst <git@dsernst.com>
2025-03-12 20:44:50 -0500

    phone screens: Remove extra y margins

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   const [items, setItems] = useState('')
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-4 row-start-2 items-start">
+    <div className="grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
+      <main className="flex flex-col gap-4 items-start">
         <Header />
 
         <Step1AddItems items={items} setItems={setItems} />
