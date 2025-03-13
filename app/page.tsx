e3118226a9b917d38806bd8124874e4cdcb55fa5
David Ernst <git@dsernst.com>
2025-03-12 19:41:17 -0500

    Expand textarea height

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         {/* Step 1: Add items */}
         <div>
           <h4>Step 1: Add your list of possible items, one per line:</h4>
-          <textarea className="w-full h-24 border border-green-800 rounded-md p-2" />
+          <textarea className="w-full h-48 border border-green-800 rounded-md p-2" />
           <div className="flex justify-end">
             <button className="bg-green-800 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-green-900">
               Save
