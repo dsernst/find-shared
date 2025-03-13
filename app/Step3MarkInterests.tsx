84d7a8c3df8a75e891bf3fe192c60bf780fb65f9
David Ernst <git@dsernst.com>
2025-03-12 20:29:55 -0500

    Make Step3MarkInterests interactive

--- app/Step3MarkInterests.tsx
+++ app/Step3MarkInterests.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
+
+type Checked = { [key: string]: boolean }
+
 export function Step3MarkInterests({ items }: { items: string }) {
+  const [checked, setChecked] = useState<Checked>({})
+
   return (
     <div>
       <div>Step 3: Mark interests</div>
       <div>
-        {items.split('\n').map((item) => (
-          <div key={item}>
-            <input type="checkbox" className="mr-2" />
-            {item}
-          </div>
-        ))}
+        {items
+          .split('\n')
+          .filter(Boolean)
+          .map((item) => (
+            <div
+              key={item}
+              className="hover:bg-white/10 rounded-md p-1 cursor-pointer"
+              onClick={() => setChecked({ ...checked, [item]: !checked[item] })}
+            >
+              <input
+                type="checkbox"
+                className="mr-2"
+                readOnly
+                checked={!!checked[item]}
+              />
+              {item}
+            </div>
+          ))}
       </div>
     </div>
   )
