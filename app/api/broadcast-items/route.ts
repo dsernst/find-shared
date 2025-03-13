dbc5c22c10c7529a0871bd01d47c4932185218d2
David Ernst <git@dsernst.com>
2025-03-13 05:29:47 -0500

    Setup pusher for sending messages from the backend

--- app/api/broadcast-items/route.ts
+++ app/api/broadcast-items/route.ts
@@ -1,9 +1,38 @@
 // nextjs api route for broadcasting items to all clients
 
 import { NextRequest, NextResponse } from 'next/server'
+import Pusher from 'pusher'
 
 export async function POST(request: NextRequest) {
-  const { items } = await request.json()
+  const { items, roomId } = await request.json()
   console.log('[items]\n\n', items)
+
+  const { pusher } = initPusher()
+
+  pusher.trigger(roomId, 'my-event', {
+    message: 'hello world',
+  })
+
   return NextResponse.json({ items })
+}
+
+function initPusher() {
+  if (!process.env.PUSHER_APP_ID)
+    throw new Error('process.env.PUSHER_APP_ID is not set')
+  if (!process.env.NEXT_PUBLIC_PUSHER_APP_KEY)
+    throw new Error('process.env.NEXT_PUBLIC_PUSHER_APP_KEY is not set')
+  if (!process.env.PUSHER_SECRET)
+    throw new Error('process.env.PUSHER_SECRET is not set')
+  if (!process.env.NEXT_PUBLIC_PUSHER_CLUSTER)
+    throw new Error('process.env.NEXT_PUBLIC_PUSHER_CLUSTER is not set')
+
+  return {
+    pusher: new Pusher({
+      appId: process.env.PUSHER_APP_ID,
+      key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+      secret: process.env.PUSHER_SECRET,
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+      useTLS: true,
+    }),
+  }
 }
