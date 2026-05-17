c295e87ba74a27efb1a4c721daaa31cce199bd7a
David Ernst <git@dsernst.com>
2026-05-17 00:13:28 -0700

    Footer: Swap 'About' / 'Github' link order

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -18,21 +18,22 @@ export const Footer = () => {
       <div className="flex flex-wrap items-center justify-center gap-x-[24px]">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/dsernst/find-shared"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Github
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href={aboutUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
           About
+        </a>
+
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/dsernst/find-shared"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+          Github
         </a>
       </div>
     </footer>
